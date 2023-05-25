a3748cdadbc5124878ce624b113180f1142147fa
Kenneth <kenneth.mburu@student.moringaschool.com>
2023-05-25 14:21:22 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,13 @@
+from PyQt6.QtWidgets import QApplication, QWidget
+
+app = QApplication(sys.argv)
+
+window = QWidget(windowTitle="Hello Kenneth")
+window.show()
+
+# app.exec()
+
+sys.exit(app.exec())
+
+
+# python3 main.py
